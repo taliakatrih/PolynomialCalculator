194cd071fbae3f51903422365db2175c8bd30739
shoval nachum <nashov@post.bgu.ac.il>
2022-04-11 22:06:57 +0300

    version 2

--- src/Calculator/Main.java
+++ src/Calculator/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("hello world111");
+        System.out.println("hello world");
+        System.out.println("shoval");
     }
 }
